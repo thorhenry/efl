3d1087a5a1b2ad454ad991efa9003f3ec6702259
Thorhenry <113508213+thorhenry@users.noreply.github.com>
2025-03-28 00:58:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3213,14 +3213,14 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 position: 9,
                 team: "Omara FC",
-                played: 0,
-                won: 0,
+                played: 1,
+                won: 1,
                 drawn: 0,
                 lost: 0,
-                gf: 0,
-                ga: 0,
-                gd: 0,
-                points: 0
+                gf: 7,
+                ga: 1,
+                gd: 6,
+                points: 3
             },
             {
                 position: 10,
@@ -3249,13 +3249,13 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 position: 12,
                 team: "Anko-Fit8",
-                played: 0,
+                played: 1,
                 won: 0,
                 drawn: 0,
-                lost: 0,
-                gf: 0,
-                ga: 0,
-                gd: 0,
+                lost: 1,
+                gf: 1,
+                ga: 7,
+                gd: -6,
                 points: 0
             },
             {
